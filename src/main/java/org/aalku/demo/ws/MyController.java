25a7db622f22e3d82d0a319d4f76fe217e784d98
aalku <aalku7@gmail.com>
2023-12-09 14:52:23 +0100

    Spring 2.1.6 and stuff

--- src/main/java/org/aalku/demo/ws/MyController.java
+++ src/main/java/org/aalku/demo/ws/MyController.java
@@ -2,7 +2,6 @@ package org.aalku.demo.ws;
 
 import java.io.IOException;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
